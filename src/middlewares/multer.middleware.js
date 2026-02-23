1e7992e4416a446e903c4313355d8e9f4849f84e
Ankush <itsankushsingh@gmail.com>
2026-02-23 20:35:32 +0530

    Multer Config

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null,"./public/temp")
+    },
+    filename: function (req, file, cb) {
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random * 1E9)
+        // cb(null,file.filename +'-'+uniqueSuffix)
+        cb(null,file.originalname)
+    }
+})
+
+const upload = multer({
+    // storage:storage // Can Also Used Like tHI
+    storage,
+})
+
+export {upload}
