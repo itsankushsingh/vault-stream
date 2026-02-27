b15e77593a13bf98932fa88e1a61678494655fec
Ankush <itsankushsingh@gmail.com>
2026-02-27 21:44:48 +0530

    Add getUserChannelProfile function to retrieve user channel profile with subscription details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -386,6 +386,77 @@ const updateUserCover = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, user, "Cover Image Updated Successfully"))
 })
 
+
+//Aggregation Pipeline For User Channel Profile
+const getUserChannelProfile = asyncHandler(async (req, res) => { 
+    const { username } = req.params;
+    
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is Required")
+    }
+    const channel = await User.aggregate([
+        {
+
+            $match: {
+                username: username?.toLowerCase()
+            }  
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers" 
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: { $size: "$subscribers" },
+                channelSubscribedToCount: { $size: "$subscribedTo" },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+    ])
+
+    if (!channel?.length === 0) {
+        throw new ApiError(404, "Channel Not Found")
+    }
+
+    return res
+        .status(200)
+        .json(
+        new ApiResponse(200, channel[0], "User Channel Profile")
+    )
+
+})
+
+
 export {
     registerUser,
     loginUser,
@@ -396,4 +467,5 @@ export {
     updateUserDetails,
     updateUserAvatar,
     updateUserCover,
+    getUserChannelProfile,
 }
--- src/models/suscription.model.js
+++ src/models/suscription.model.js
@@ -1,10 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
 const SuscriptionSchema = new Schema({
+    
     subscriber: {
         type: Schema.Types.ObjectId, // One Who Is Subscribing
         ref: "User",
     },
+
     channel: {
         type: Schema.Types.ObjectId, // One Who Is Being Subscribed To
         ref: "User",
